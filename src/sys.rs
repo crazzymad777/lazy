97f067aa4cbbedac43c2bb780fafb9071fd4a14c
Yury Moskov <aerobus.778@gmail.com>
2024-10-29 17:39:07 +0300

    use libc instead of syscalls

--- src/sys.rs
+++ src/sys.rs
@@ -1,15 +1,25 @@
 pub fn reboot(response: String) {
+ //    unsafe {
+ //        use syscalls::syscall;
+	// let cmd = match response.as_str() {
+ //            "poweroff" => 0x4321fedcusize,
+ //            "restart" => 0x01234567usize,
+ //            "halt" => 0xcdef0123usize,
+	//     &_ => todo!()
+	// };
+ //        if let Err(e) = syscall(syscalls::SYS_reboot, &syscalls::SyscallArgs::from(&[0xfee1dead, 537993216, cmd])) {
+ //            println!("{}", e);
+ //        }
+ //    }
     unsafe {
-        use syscalls::syscall;
-	let cmd = match response.as_str() {
-            "poweroff" => 0x4321fedcusize,
-            "restart" => 0x01234567usize,
-            "halt" => 0xcdef0123usize,
-	    &_ => todo!()
-	};
-        if let Err(e) = syscall(syscalls::SYS_reboot, &syscalls::SyscallArgs::from(&[0xfee1dead, 537993216, cmd])) {
-            println!("{}", e);
-        }
+        use libc::reboot;
+        let cmd = match response.as_str() {
+            "poweroff" => libc::RB_POWER_OFF,
+            "restart" => libc::RB_AUTOBOOT,
+            "halt" => libc::RB_HALT_SYSTEM,
+            &_ => todo!()
+        };
+        reboot(cmd);
     }
 }
 
