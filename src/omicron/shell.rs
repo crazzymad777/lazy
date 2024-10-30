746f7b82b2a960eea798be210b1ef46fe2ab6f16
Yury Moskov <aerobus.778@gmail.com>
2024-10-30 16:20:25 +0300

    much better parser

--- src/omicron/shell.rs
+++ src/omicron/shell.rs
@@ -2,7 +2,32 @@ use crate::omicron::command;
 
 pub fn run(_command: &str) -> Result<command::Process, String> {
     use crate::omicron::command::CommandBuilder;
+    let memory = String::with_capacity(256);
+
     let mut builder = CommandBuilder::new();
+    let mut i = 0;
+    let len = _command.len();
+    let mut toggle = false;
+    while i < len {
+        let x = _command[i];
+        if x == ' ' {
+            memory.push('\0');
+            if (toggle) {
+                builder.arg(memory);
+            } else {
+                builder.program(memory);
+                toggle = true;
+            }
+            memory = String::from("");
+        } else {
+            s.push(x);
+        }
+        i = i + 1;
+    }
+
+    if (memory != "") {
+        builder.arg(memory);
+    }
 
     builder.spawn()
 }
