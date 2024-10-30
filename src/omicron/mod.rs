829e82487e2a78e2bd93533222571606e668db01
Yury Moskov <aerobus.778@gmail.com>
2024-10-30 19:17:36 +0300

    test Cstr

--- src/omicron/mod.rs
+++ src/omicron/mod.rs
@@ -32,3 +32,38 @@ impl Process {
         }
     }
 }
+
+#[cfg(test)]
+mod testsCstr {
+    use crate::omicron::utils::Cstr;
+    use super::*;
+
+    #[test]
+    #[should_panic]
+    fn test_check_panic() {
+        Cstr::check("non-null-terminated-string").ok().unwrap();
+    }
+
+    #[test]
+    fn test_check() {
+        let x = "null-terminated-string\0";
+        let expected = x;
+        let actual = Cstr::check(x).ok().unwrap();
+        assert_eq!(expected, actual);
+    }
+
+    #[test]
+    #[should_panic]
+    fn test_new_magic_panic() {
+        Cstr::new_magic("non-null-terminated-string");
+    }
+
+    #[test]
+    fn test_magic() {
+        let x = "null-terminated-string\0";
+        let expected = x.as_ptr() as *const libc::c_char;
+        let actual = Cstr::magic(x);
+        assert_eq!(expected, actual);
+    }
+}
+
