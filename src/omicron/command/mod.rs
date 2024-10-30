c792edf658bdb18170c71e218cc9e032aebee74c
Yury Moskov <aerobus.778@gmail.com>
2024-10-30 19:00:29 +0300

    cover parser with tests

--- src/omicron/command/mod.rs
+++ src/omicron/command/mod.rs
@@ -1,7 +1,66 @@
-pub mod Builder;
-pub mod Parser;
+pub mod builder;
+pub mod parser;
 mod utils;
 
-pub use crate::omicron::command::Builder::CommandBuilder;
-pub use crate::omicron::command::Parser::CommandParser;
-pub use crate::omicron::command::Parser::parse;
+pub use crate::omicron::command::builder::CommandBuilder;
+pub use crate::omicron::command::parser::CommandParser;
+pub use crate::omicron::command::parser::parse;
+
+#[cfg(test)]
+mod tests {
+    // Note this useful idiom: importing names from outer (for mod tests) scope.
+    use super::*;
+
+    #[test]
+    fn test_parser_empty() {
+        let expected = CommandBuilder::new().to_string();
+        let actual = parse("").to_string();
+        assert_eq!(expected, actual);
+    }
+
+    #[test]
+    fn test_parser_args() {
+        let expected = CommandBuilder::new()
+            .program("hello\0")
+            .arg("1\0")
+            .arg("2\0")
+            .arg("3\0")
+            .to_string();
+        let actual = parse("hello 1 2 3").to_string();
+        assert_eq!(expected, actual);
+    }
+
+    #[test]
+    fn test_parser_single_quotes() {
+        let expected = CommandBuilder::new()
+            .program("hello\0")
+            .arg("1 2 3\0")
+            .arg("4 5 6\0")
+            .to_string();
+        let actual = parse("hello '1 2 3' '4 5 6'").to_string();
+        assert_eq!(expected, actual);
+    }
+
+    #[test]
+    fn test_parser_escape_single_quote() {
+        let expected = CommandBuilder::new()
+            .program("hello\0")
+            .arg("'1\0")
+            .arg("2\0")
+            .arg("3'\0")
+            .arg("4 5 6\0")
+            .to_string();
+        let actual = parse("hello \\'1 2 3\\' '4 5 6'").to_string();
+        assert_eq!(expected, actual);
+    }
+
+    #[test]
+    fn test_parser_escape_in_single_quotes() {
+        let expected = CommandBuilder::new()
+            .program("hello\0")
+            .arg("1 2 3\\\0")
+            .to_string();
+        let actual = parse("hello '1 2 3\\'").to_string();
+        assert_eq!(expected, actual);
+    }
+}
---
+++ src/omicron/command/parser.rs
@@ -0,0 +1,88 @@
+use crate::omicron::command::CommandBuilder;
+use crate::omicron::command;
+
+pub struct CommandParser {
+    memory: String, // use raw bytes?
+    builder: CommandBuilder,
+    toggle: bool,
+    in_single_quotes: bool,
+    escape: bool
+}
+
+impl CommandParser {
+    pub fn new() -> CommandParser {
+        CommandParser {
+            memory: String::with_capacity(256),
+            builder: CommandBuilder::new(),
+            toggle: false,
+            in_single_quotes: false,
+            escape: false
+        }
+    }
+
+    fn load(&mut self) {
+        self.memory.push('\0');
+        if self.toggle {
+            self.builder.arg(&self.memory);
+        } else {
+            self.builder.program(&self.memory);
+            self.toggle = true;
+        }
+    }
+
+    pub fn feed_char(&mut self, x: char) {
+        if self.in_single_quotes {
+            self.feed_char_with_sepataror(x, '\'');
+        } else {
+            self.feed_char_with_sepataror(x, ' ');
+        }
+    }
+
+    pub fn feed_char_with_sepataror(&mut self, x: char, separator: char) {
+        if x == separator {
+            if x == ' ' {
+                self.load();
+                self.memory = String::from("");
+            }
+            if x == '\'' {
+                self.in_single_quotes = false;
+            }
+        } else {
+            if x == '\'' {
+                if self.escape {
+                    self.escape = false;
+                    self.memory.push(x);
+                } else {
+                    self.in_single_quotes = true
+                }
+            } else {
+                if x == '\\' && !self.in_single_quotes {
+                    if self.escape {
+                        self.escape = false;
+                        self.memory.push(x);
+                    } else {
+                        self.escape = true;
+                    }
+                } else {
+                    self.memory.push(x);
+                }
+            }
+        }
+    }
+
+    pub fn finish(mut self) -> CommandBuilder {
+        if self.memory != "" {
+            self.load();
+        }
+        self.builder
+    }
+}
+
+pub fn parse(_command: &str) -> CommandBuilder {
+    let mut parser = CommandParser::new();
+
+    for x in _command.chars() {
+        parser.feed_char(x);
+    }
+    parser.finish()
+}
