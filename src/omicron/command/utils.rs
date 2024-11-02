88b1bf44abdbd27f71ce8bb8cf81dbdf8813b48a
Yury Moskov <aerobus.778@gmail.com>
2024-11-02 03:00:26 +0300

    Omicron: duplicate file descriptors

--- src/omicron/command/utils.rs
+++ src/omicron/command/utils.rs
@@ -6,23 +6,6 @@ use crate::omicron::Process;
 // 0 - for child
 // <0 - error
 // >0 - parent, Process::new()
-
-pub unsafe fn fopen_write_to_null() -> *mut libc::FILE {
-    use crate::omicron::utils::Cstr;
-    libc::fopen(
-        Cstr::new_magic("/dev/null\0"),
-        Cstr::new_magic("w\0")
-    )
-}
-
-pub unsafe fn fopen_read_from_null() -> *mut libc::FILE {
-    use crate::omicron::utils::Cstr;
-    libc::fopen(
-        Cstr::new_magic("/dev/null\0"),
-        Cstr::new_magic("r\0")
-    )
-}
-
 pub unsafe fn execute(program: &str, ptr_args: &Vec<*const i8>, new_group: bool) -> Result<Process, String> {
     use crate::omicron::utils::errno_to_string;
     use crate::omicron::utils::Cstr;
@@ -46,22 +29,16 @@ pub unsafe fn execute(program: &str, ptr_args: &Vec<*const i8>, new_group: bool)
     // result = 0
     unsafe {
         // redirect stdio
-        
-        let file_out = fopen_write_to_null();
-        let new_fd_out = libc::fileno(file_out);
+        let new_fd_out = libc::open(Cstr::new_magic("/dev/null\0"), libc::O_WRONLY);
+        let new_fd_in = libc::open(Cstr::new_magic("/dev/null\0"), libc::O_RDONLY);
 
-        let file_in = fopen_write_to_null();
-        let new_fd_in = libc::fileno(file_in);
+        libc::close(libc::STDOUT_FILENO);
+        libc::close(libc::STDERR_FILENO);
+        libc::close(libc::STDIN_FILENO);
 
-        // Not Thread Safe:
-        // libc::fclose(libc::STDOUT_FILENO as *mut FILE);
-        // libc::fclose(libc::STDERR_FILENO as *mut FILE);
-        // libc::fcntl(newFd, libc::F_DUPFD, libc::STDOUT_FILENO);
-        // libc::fcntl(newFd, libc::F_DUPFD, libc::STDERR_FILENO);
-
-        libc::dup2(new_fd_out, libc::STDOUT_FILENO);
-        libc::dup2(new_fd_out, libc::STDERR_FILENO);
-        libc::dup2(new_fd_in, libc::STDIN_FILENO);
+        libc::fcntl(new_fd_out, libc::F_DUPFD, libc::STDOUT_FILENO);
+        libc::fcntl(new_fd_out, libc::F_DUPFD, libc::STDERR_FILENO);
+        libc::fcntl(new_fd_in, libc::F_DUPFD, libc::STDIN_FILENO);
     }
     let _error = libc::execvp(Cstr::magic(program), ptr_args.as_ptr());
     panic!("execvp failed: {}", errno_to_string().unwrap_or("execv failed".to_string())) // child panic
